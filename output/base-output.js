ea9a7a8651f208b375ef7131ac5c6b2a1c0599db
terric <terric.ying@gmail.com>
2026-06-15 09:56:37 +0800

    feat(output): 添加输出颜色辅助工具

--- output/base-output.js
+++ output/base-output.js
@@ -11,6 +11,30 @@ class BaseOutput {
         this.stream.write(text + '\n');
     }
 
+    // ANSI颜色代码
+    static colors = {
+        reset: '\x1b[0m',
+        red: '\x1b[31m',
+        green: '\x1b[32m',
+        yellow: '\x1b[33m',
+        blue: '\x1b[34m',
+        magenta: '\x1b[35m',
+        cyan: '\x1b[36m',
+        white: '\x1b[37m',
+        gray: '\x1b[90m',      // 浅灰色
+        lightGreen: '\x1b[92m', // 浅绿色
+        lightRed: '\x1b[91m',   // 浅红色
+        lightYellow: '\x1b[93m', // 浅黄色
+        lightBlue: '\x1b[94m',  // 浅蓝色
+        lightMagenta: '\x1b[95m', // 浅洋红色
+        lightCyan: '\x1b[96m',  // 浅青色
+    };
+
+    // 颜色包装方法
+    colorize(text, color) {
+        return `${color}${text}${BaseOutput.colors.reset}`;
+    }
+
     render() {
         throw new Error('子类必须实现 render 方法');
     }
